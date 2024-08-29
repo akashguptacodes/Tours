cdef88a2ef67db00f8a7124ad204400b3ed7e595
Akash <akashgupta7484@gmail.com>
2024-08-29 11:05:39 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info: "The Taj Mahal! A breathtaking monument of love and architecture. Located in Agra, India, it's a stunning example of Mughal design and craftsmanship. Built between 1632 and 1653 by Emperor Shah Jahan as a tribute to his beloved wife, Mumtaz Mahal, the Taj Mahal is considered one of the Seven Wonders of the World.",
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price: "₹ 35,758",
+    },
+
+    {
+        id: 2,
+        name: "Jaipur",
+        info: "Jaipur is the vibrant capital of India's Rajasthan state, known for its stunning architecture, rich history, and vibrant culture. Founded in 1727 by Maharaja Sawai Jai Singh II, the city is often referred to as the 'Pink City' due to its distinctive pink-hued buildings. Jaipur is famous for its iconic landmarks such as the Hawa Mahal, City Palace, and Jantar Mantar.",
+        image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price: "₹ 46,758",
+    },
+
+    {
+        id: 3,
+        name: "Goa",
+        info: "Goa! A popular tourist destination in India known for its vibrant beaches, rich cultural heritage, and laid-back atmosphere. Located on the west coast, Goa is a former Portuguese colony that offers a unique blend of Indian and Western influences. The state is famous for its beautiful beaches, such as Baga, Calangute, and Palolem, as well as its nightlife, water sports, and seafood.",
+        image: "https://cdn.thecodehelp.in/Goa.jpeg",
+        price: "₹ 67,758",
+    },
+
+    {
+        id: 4,
+        name: "Varanasi",
+        info: "Varanasi, also known as Benares, is a ancient city located in the state of Uttar Pradesh in northern India. It is situated on the banks of the Ganges River and is considered one of the holiest cities in Hinduism. Varanasi is a significant pilgrimage site, drawing millions of devotees each year to its numerous temples, ghats, and shrines. ",
+        image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price: "₹ 48,758",
+    },
+
+    {
+        id: 5,
+        name: "Darjeeling",
+        info: "Darjeeling is a popular hill station in the Indian state of West Bengal, known for its majestic Himalayan backdrop, lush green tea gardens, and pleasant climate. The name 'Darjeeling' is derived from the word 'Darj' meaning 'thunderbolt' and 'Jeen' meaning 'plain' in Tibetan. The town is famous for its Darjeeling Himalayan Railway, a UNESCO World Heritage Site, and its tea production, which is considered some of the world's best.",
+        image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price: "₹ 56,758",
+    },
+
+    {
+        id: 6,
+        name: "Jaisalmer",
+        info: "Jaisalmer! A charming city in the Indian state of Rajasthan, known for its stunning architecture, rich cultural heritage, and breathtaking natural beauty. Often referred to as the 'Golden City' due to the yellow sandstone buildings, Jaisalmer is a popular tourist destination. Visit the Jaisalmer Fort, a UNESCO World Heritage Site, and explore the narrow alleys and bustling markets. ",
+        image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price: "₹ 55,758",
+    },
+
+    {
+        id: 7,
+        name: "Kochi",
+        info: "Kochi is the legislative capital of Kerala, a state in southern India. Located on the coast of the Arabian Sea, Kochi is one of the oldest and most cosmopolitan cities in India. The city has a rich history, dating back to the 13th century, and is known for its pristine beaches, picturesque backwaters, and cultural heritage. ",
+        image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+        price: "₹ 34,758",
+    }
+];
+
+export default data;
